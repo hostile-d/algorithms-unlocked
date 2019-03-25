17b4783f0fffbe5ab9505e7af7128da9136b6b73
hostile_d <host.chel@gmail.com>
2019-03-25 19:28:31 +0500

    update classes

--- Solutions/cs/AlgorithmsUnlocked/AlgorithmsUnlocked/Algorithms/ModularExponentiation.cs
+++ Solutions/cs/AlgorithmsUnlocked/AlgorithmsUnlocked/Algorithms/ModularExponentiation.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AlgorithmsUnlocked.Algorithms
 {
-    class ModularExponentiation
+    public class ModularExponentiation
     {
         public static int Run(int x, int d, int n)
         {
